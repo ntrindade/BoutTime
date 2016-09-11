2f477f2ee801979bf106da1178fa755bec3a175d
Nuno Trindade <nunomtrindade@gmail.com>
2016-09-11 22:29:45 +0200

    [Bout Time] Implements the UI

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelEvent1: UILabel!
+    @IBOutlet weak var labelEvent2: UILabel!
+    @IBOutlet weak var labelEvent3: UILabel!
+    @IBOutlet weak var labelEvent4: UILabel!
+
+    @IBOutlet weak var buttonFullDown: UIButton!
+    @IBOutlet weak var buttonHalfUpOne: UIButton!
+    @IBOutlet weak var buttonHalfDownOne: UIButton!
+    @IBOutlet weak var buttonHalfUpTwo: UIButton!
+    @IBOutlet weak var buttonHalfDownTwo: UIButton!
+    @IBOutlet weak var buttonFullUp: UIButton!
+    
+    @IBOutlet weak var labelTime: UILabel!
+    @IBOutlet weak var labelInfo: UILabel!   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Action Methods
+    
+    @IBAction func MoveUp(sender: UIButton) {
+    
+    }
+    
+    @IBAction func MoveDown(sender: UIButton) {
+    
+    }
+    
     // MARK: Helper Methods
     
     func showAlert(title: String, message: String? = nil, style: UIAlertControllerStyle = .Alert) {
